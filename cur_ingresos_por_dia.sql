99e803fc957f633b9686c0fa598c56678df3189b
Suarez1717 <byronsuarez1706@gmail.com>
2025-08-26 22:06:19 -0600

    Delete cur_ingresos_por_dia.sql

--- cur_ingresos_por_dia.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE PROCEDURE cur_ingresos_por_dia(p_rc OUT SYS_REFCURSOR) AS
-BEGIN
-  OPEN p_rc FOR
-    SELECT TRUNC(fecha_venta) fecha, SUM(cantidad*precio) ingresos
-    FROM ventas GROUP BY TRUNC(fecha_venta) ORDER BY fecha;
-END;
-/
