6226a1efbb7619056c05182ac3c2931cd55f1070
Suarez1717 <byronsuarez1706@gmail.com>
2025-08-26 22:06:26 -0600

    Delete cur_buscar_usuario.sql

--- cur_buscar_usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PROCEDURE cur_buscar_usuario(p_texto IN VARCHAR2, p_rc OUT SYS_REFCURSOR) AS
-BEGIN
-  OPEN p_rc FOR
-    SELECT * FROM usuarios
-    WHERE UPPER(nombre) LIKE '%'||UPPER(p_texto)||'%'
-       OR UPPER(rol)    LIKE '%'||UPPER(p_texto)||'%';
-END;
-/
