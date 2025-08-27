041c8671329cb4803deecddad157bbae1df32b45
Suarez1717 <byronsuarez1706@gmail.com>
2025-08-26 22:05:50 -0600

    Delete cur_productos_con_precio_invalido.sql

--- cur_productos_con_precio_invalido.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE PROCEDURE cur_productos_con_precio_invalido(p_rc OUT SYS_REFCURSOR) AS
-BEGIN
-  OPEN p_rc FOR
-    SELECT * FROM productos WHERE precio IS NULL OR precio < 0;
-END;
-/
