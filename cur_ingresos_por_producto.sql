0f5b782865cb3633450f7eff217c45d77bb9fa7a
Suarez1717 <byronsuarez1706@gmail.com>
2025-08-26 22:06:13 -0600

    Delete cur_ingresos_por_producto.sql

--- cur_ingresos_por_producto.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PROCEDURE cur_ingresos_por_producto(p_rc OUT SYS_REFCURSOR) AS
-BEGIN
-  OPEN p_rc FOR
-    SELECT p.id id_producto, p.nombre, NVL(SUM(v.cantidad*v.precio),0) ingresos
-    FROM productos p LEFT JOIN ventas v ON v.id_producto = p.id
-    GROUP BY p.id, p.nombre ORDER BY ingresos DESC;
-END;
-/
